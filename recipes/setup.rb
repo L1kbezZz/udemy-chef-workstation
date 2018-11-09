f54b25b71207f038c6cad1958f1ae482ada1a86e
vagrant <vagrant@localhost.localdomain>
2018-11-09 15:09:56 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,21 @@
+package 'ntp' do
+  action :install
+end
+
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do
+  content "This server is the property of Roman Kuzmenko\n"
+  group 'root'
+  owner 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
